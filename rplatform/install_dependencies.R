aeaf425ade1cbac30a2c3dc02bbe6271ea91ca39
Konrad Zieliński <konrad.zielinski@contractors.roche.com>
2019-07-10 10:03:31 +0200

    demo01

--- rplatform/install_dependencies.R
+++ rplatform/install_dependencies.R
@@ -1,10 +1,10 @@
-# RP package template version >= 0.0.70
+# RP package template version >= 0.0.77
 
 # The base image makes use of the stable GRAN repository corresponding to image's R version as a source of packages. 
 # If you wish to install custom package versions from other source you'll need to
 # modify the "repos" option or provide the repository url explicitly during package installation.
 
-# To install and verify custom packages use rp::installAndVerify function.
+# To install and verify packages use rp::installAndVerify function.
 # Version requirements can be defined with "requirement" argument:
 # -- install = install.packages
 # '*' - any version
@@ -14,9 +14,19 @@
 # 'r:tag_name' - reference github tag
 # 's:sha1' - github SHA1
 
-# Install all dependencies for packages from directory defined with project.sources argument.
-# Use dont.install argument to define packages which should be ommited i.e. packages installed with
-# rp::installAndVerify function.
-rp::installDependencies(project.sources = "/mnt/vol", 
-                        dont.install = c())
+# Install packages using multiple cores
+if (!require(parallel)) rp::installAndVerify(package = "parallel", requirement = "*")
+options(Ncpus = parallel::detectCores())
+
+pkgs_to_install <- c(
+  # Add your dependencies here
+)
+rp::installAndVerify(package = pkgs_to_install)
+
+# Or uncomment following and use desc::desc_get_deps() to extract dependencies from DESCRIPTION file
+# deps <- desc::desc_get_deps(file.path("/mnt/vol", "path/to/DESCRIPTION"))
+# rp::installAndVerify(install = install.packages,
+#                      package = deps$package,
+#                      requirement = deps$version)
+
 
